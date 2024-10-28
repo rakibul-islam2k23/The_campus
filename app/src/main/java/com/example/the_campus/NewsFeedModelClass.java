037f5909e972cc8c4fa33dbd921ff260ae3abbcc
sharif <mohammadrakibul2k23@gmail.com>
2024-10-29 01:24:57 +0600

    News Feed Complete

---
+++ app/src/main/java/com/example/the_campus/NewsFeedModelClass.java
@@ -0,0 +1,20 @@
+package com.example.the_campus;
+
+public class NewsFeedModelClass {
+    String post;
+
+    public NewsFeedModelClass() {
+    }
+
+    public NewsFeedModelClass(String post) {
+        this.post = post;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+}
